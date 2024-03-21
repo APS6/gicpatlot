eac1a7b6957debd0e4becbd41778bead336fe5d3
aps <anirudhapsah@gmail.com>
2024-03-21 11:56:04 +0530

    new name, minor additions

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   site: 'https://www.gicpatlot.in',
   integrations: [tailwind()],
+  output: 'hybrid',
   adapter: vercel({
     webAnalytics: { enabled: true }
   }),
