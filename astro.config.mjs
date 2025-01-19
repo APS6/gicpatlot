2971c9bd0d47cf66a3f1ed89e20669a81ef92ec6
aps <anirudhapsah@gmail.com>
2025-01-19 14:12:37 +0530

    updated packages and removed vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
-import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.gicpatlot.in',
+  site: "https://www.gicpatlot.in",
   integrations: [tailwind()],
-  output: 'hybrid',
-  adapter: vercel({
-    webAnalytics: { enabled: true }
-  }),
+  output: "static",
 });
